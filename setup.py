28d172ff022aee8b49af65a4c115ebd76662ec75
Ismavreverse <bgautam.0707@gmail.com>
2017-12-09 11:07:44 +0530

    initial code

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cogvi',
+  packages = ['cogvi'], # this must be the same as the name above
+  version = '0.1',
+  description = 'AI Lib',
+  author = 'Gautam A',
+  author_email = 'bgautam0707@gmail.com',
+  url = 'https://github.com/gautam0707/cogvi', # use the URL to the github repo
+  download_url = 'https://github.com/gautam0707/cogvi/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['ai', 'agi', 'ml'], # arbitrary keywords
+  classifiers = [],
+)
